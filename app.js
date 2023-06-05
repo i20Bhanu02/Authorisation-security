9d7884b5d643907528ab23f9469f36ef25030f34
Bhavesh Pant <20bhaveshpant02@gmail.com>
2023-06-05 18:52:17 +0530

    added cookie functionality using passport

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltrounds = 5;
+const session = require("express-session");
+const passport = require("passport");
+const passportlocalmongoose = require("passport-local-mongoose");
+
 
 const app=express();
 
@@ -12,16 +14,31 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({extended:true}));
 
+app.use(session({
+    secret : "its a secret",
+    resave : false,
+    saveUninitialized : false,
+})); 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.set('bufferCommands', false);
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+// mongoose.set("use/")
 
 const userSchema = new mongoose.Schema( {
     email : String,
     password : String,
 });
 
+userSchema.plugin(passportlocalmongoose);
 
 const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
@@ -38,39 +55,77 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
     
-    bcrypt.hash(req.body.password, saltrounds, function(err, hash) {
-        if(err) console.log(err);
-        else{
-            const newuser = new User({
-                email : req.body.username,
-                password : hash,
-            });
+    // bcrypt.hash(req.body.password, saltrounds, function(err, hash) {
+    //     if(err) console.log(err);
+    //     else{
+    //         const newuser = new User({
+    //             email : req.body.username,
+    //             password : hash,
+    //         });
         
-            newuser.save().then(() =>{
-                res.render("secrets");
-            }).catch((err)=>{
-                console.log(err);
-            });
+    //         newuser.save().then(() =>{
+    //             res.render("secrets");
+    //         }).catch((err)=>{
+    //             console.log(err);
+    //         });
+    //     }
+    // });
+
+    User.register({username : req.body.username} , req.body.password , function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/");
         }
-    });
-    
-    
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
 });
+
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else{
+        res.redirect("/login");
+    }
+})
 
 app.post("/login",function(req,res){
 
-    User.findOne({email : req.body.username}).then((found)=>{
-        if(found){
-            bcrypt.compare(req.body.password, found.password, function(err, result) {
-                if(result) res.render("secrets");
-                else res.send("Invalid Password");
-            });
+    // User.findOne({email : req.body.username}).then((found)=>{
+    //     if(found){
+    //         bcrypt.compare(req.body.password, found.password, function(err, result) {
+    //             if(result) res.render("secrets");
+    //             else res.send("Invalid Password");
+    //         });
+    //     }
+    //     else res.send("Username doesnot exist, please register");
+    // }).catch((err)=>{
+    //     console.log(err);
+    // })
+    const newuser = new User({
+        username : req.body.username,
+        password : req.body.password,
+    });
+
+    req.login(newuser,function(err){
+        if(err) console.log("f");
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
         }
-        else res.send("Username doesnot exist, please register");
-    }).catch((err)=>{
-        console.log(err);
     })
-})
+});
+
+app.get("/logout",function(req,res){
+    req.logout(function(err){
+        if(err) console.log(err);
+        else res.redirect("/");
+    });
+});
 
 
 app.listen("4000",function(){
