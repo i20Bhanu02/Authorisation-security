4127664c60d89b0605e264bf29a01bba1cd95360
Bhavesh Pant <20bhaveshpant02@gmail.com>
2023-06-05 14:36:43 +0530

    Created lvl1 security by saving username pass

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyparser.urlencoded({extended:true}));
+
+mongoose.set('bufferCommands', false);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+    email : String,
+    password : String,
+};
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+
+app.post("/register",function(req,res){
+    const newuser = new User({
+        email : req.body.username,
+        password : req.body.password,
+    });
+
+    newuser.save().then(() =>{
+        res.render("secrets");
+    }).catch((err)=>{
+        console.log(err);
+    });
+    
+});
+
+app.post("/login",function(req,res){
+
+    User.findOne({email : req.body.username}).then((found)=>{
+        if(found){
+            if(found.password === req.body.password) res.render("secrets");
+            else res.send("Invalid Password");
+        }
+        else res.send("Username doesnot exist, please register");
+        
+        
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
+app.listen("4000",function(){
+    console.log("done");
+});
+
